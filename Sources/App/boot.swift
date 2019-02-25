214af2a80f0e41efba31c4aa2718bdae249267ea
Remi Groult <>
2019-02-25 22:31:20 +0100

    fix context

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -1,6 +1,23 @@
 import Vapor
+import Meow
 
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
     // Your code here
+   // let context:Meow.Context = try app.make()
+    try app.make(Future<Meow.Context>.self)
+        .whenSuccess({ context in
+            print("Context \(context)")
+            do {
+                print("Nbre of uers \(try context.count(User.self).wait())")
+                //executeAndComplete(try context.count(User.self))
+            }catch{
+                
+            }
+            
+            context.find(User.self).getAllResults()
+                .whenSuccess { users in
+                    print("User \(users.count)")
+            }
+        })
 }
