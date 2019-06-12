60efd09c9bb40c0e3064a9d5244cf626e4d1efe8
Remi Groult <>
2019-06-12 21:20:05 +0200

    tokenInfo tests

--- Tests/AppTests/Services/TokenInfoServiceTests.swift
+++ Tests/AppTests/Services/TokenInfoServiceTests.swift
@@ -14,9 +14,45 @@ import Meow
 final class TokenInfoServiceTests: BaseAppTests {
     func testCreateToken() throws {
         let value = ["Hello":"World"]
-        let tokenId = store(info: value, durationInSecs: 2, into: context)
+        let tokenId = try store(info: value, durationInSecs: 2, into: context).wait()
         XCTAssertNotNil(tokenId)
     }
     
+    func testCreateAndRetrieveToken() throws {
+        let value = ["Hello":"World"]
+        let tokenId = try store(info: value, durationInSecs: 2, into: context).wait()
+        
+        let retrieveValue = try findInfo(with: tokenId, into: context).wait()
+        
+        XCTAssertEqual(value, retrieveValue)
+    }
     
+    func testExpiredDuration() throws{
+        let value = ["Hello":"World"]
+        let tokenId = try store(info: value, durationInSecs: 2, into: context).wait()
+        sleep(4)
+        let retrieveValue = try findInfo(with: tokenId, into: context).wait()
+        XCTAssertNil(retrieveValue)
+    }
+    
+    func testExpiredPurge() throws{
+        let value = ["Hello":"World"]
+        _ = try store(info: value, durationInSecs: 2, into: context).wait()
+        sleep(4)
+        var purged = try purgeExpiredTokens(into: context).wait()
+        XCTAssertEqual(purged, 1)
+        
+        purged = try purgeExpiredTokens(into: context).wait()
+        XCTAssertEqual(purged, 0)
+    }
+    
+    func testPurgeAll() throws{
+        let value = ["Hello":"World"]
+        for _ in 0..<100 {
+            _ = try store(info: value, durationInSecs: 2, into: context).wait()
+        }
+        let purged = try purgeAllTokens(into: context).wait()
+        XCTAssertEqual(purged, 100)
+        
+    }
 }
