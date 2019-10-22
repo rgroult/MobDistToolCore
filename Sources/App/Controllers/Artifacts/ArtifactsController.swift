54dc59864f6097a121858accc28fa9edcd9f6ce1
Rémi Groult <>
2019-10-22 13:35:53 +0200

    remove unused code

--- Sources/App/Controllers/Artifacts/ArtifactsController.swift
+++ Sources/App/Controllers/Artifacts/ArtifactsController.swift
@@ -22,7 +22,7 @@ enum customHeadersName:String {
 }
 
 final class ArtifactsController:BaseController  {
-  
+    
     let maxUploadSize = 1024*1024*1024*1024
     
     init(apiBuilder:OpenAPIBuilder) {
@@ -52,13 +52,6 @@ final class ArtifactsController:BaseController  {
                 case .ios:
                     guard mimeType == IPA_CONTENT_TYPE else { throw ArtifactError.invalidContentType}
                 }
-                
-                
-                ///         let stream = try req.fileio().chunkedStream(file: "/path/to/file.txt")
-                ///         var res = HTTPResponse(status: .ok, body: stream)
-                ///         res.contentType = .plainText
-                ///         return res
-                ///     }
                 
                 //already Exist
                 return try isArtifactAlreadyExist(app: app, branch: branch, version: version, name: artifactName, into: context)
@@ -105,7 +98,7 @@ final class ArtifactsController:BaseController  {
         let branch = try req.parameters.next(String.self)
         let version = try req.parameters.next(String.self)
         let artifactName = try req.parameters.next(String.self)
-       
+        
         return try deleteArtifactWithInfo(req, apiKey: apiKey, branch: branch, version: version, artifactName: artifactName)
     }
     
@@ -196,8 +189,8 @@ final class ArtifactsController:BaseController  {
                         let manifest = ArtifactsController.generateiOsManifest(absoluteIpaUrl: downloadUrl, bundleIdentifier: bundleID, bundleVersion: bundleVersion, ApplicationName: name)
                         
                         return req.response(manifest, as: .xml)
-                    }
-            }
+                }
+        }
     }
     
     //GET {idArtifact}/file?token='
@@ -212,122 +205,36 @@ final class ArtifactsController:BaseController  {
                 guard let info = info, let id = info[ArtifactTokenKeys.artifactId.rawValue] else { throw  Abort(.badRequest, reason: "Token not found or expired") }
                 return try findArtifact(byID: id, into: context)
                 // throw "not implemented"
-            }
-            .flatMap{ artifact in
-                guard let artifact = artifact else { throw  Abort(.serviceUnavailable, reason: "Artifact not found for ID") }
-                return try retrieveArtifactData(artifact: artifact, storage: storage, into: context)
-                //return req.response("not implemented", as: .xml)
-                    .flatMap{ storeResult  in
-                      /*  guard let mediaType = MediaType.parse(artifact.contentType ?? "") else { throw  Abort(.internalServerError, reason: "invalid Artifact mime Type") }
-                        response.http.contentType = mediaType*/
-                        let response:Response
-                        switch storeResult {
-                        case .asFile(let file):
-                            ()
-                            response = req.response("not implemented", as: .xml)
-                            //TO DO
-                        case .asUrI(let url):
-                            if url.scheme == "file"{ //local files
-                                return try req.streamFile(at: url.absoluteString)
-                            }else {
-                                //redirect to it
-                                response = req.redirect(to: url.absoluteString)
-                            }
-                        }
-                        return req.eventLoop.newSucceededFuture(result: response)
-                        //let response = req.response()
-                       
-                        //response.http.body = 
-                     //   return req.response("not implemented", as: .xml)
-                        //throw "not implemented"
-                }
-            }
-        
-    }
-    /*
- var body = new Body(stream);
- var headers = {"Content-Type":artifact.contentType,"Content-length":"${artifact.size}","Content-Disposition":"attachment; filename=${artifact.filename}"};
- var response = new shelf.Response(200,body:body,headers:headers);
- */
-    //PUT 'artifacts/{idArtifact}/
-    
-/*enum StoredResult {
- case asFile(file:Foundation.FileHandle)
- case asUrI(url:URL)
- }*/
-    
-    /*return try req.content.decode(ArtifactCreateUpdateDto.self)
-     .flatMap({ artifactCreateUpdateDto in
-     return try findApplication(apiKey: apiKey, into: context)
-     .flatMap({ app  in
-     guard let app = app else { throw ApplicationError.notFound }
-     return try createArtifact(app: app, name: artifactCreateUpdateDto.name, version: artifactCreateUpdateDto.version, branch: artifactCreateUpdateDto.branch, sortIdentifier: artifactCreateUpdateDto.sortIdentifier, tags: artifactCreateUpdateDto.metaDataTags, into: context)})})
-     .map{ArtifactDto(from: $0, content: .full)}
-     */
-    
-    /*let uuid = try req.parameters.next(UUID.self)
-     let context = try /Users/rgroult/Developments/Perso/MobDistToolSwift/Sources/App/Controllers/Artifacts/ArtifactsController+Routing.swiftreq.context()
-     return try retrieveUser(from:req)
-     .flatMap{user -> Future<ApplicationDto> in
-     guard let user = user else { throw Abort(.unauthorized)}
-     return try req.content.decode(ApplicationUpdateDto.self)*/
-    
-    //POST 'artifacts/{appUUID} // path: 'artifacts/{apiKey}/{_branch}/{_version}/{_artifactName}')
-    //PUT 'artifacts/{idArtifact}/info
-    //PUT 'artifacts/{idArtifact}/file
-    //GET 'artifacts/{idArtifact}/info
-    //GET 'artifacts/{idArtifact}/file
-    //DELETE 'artifacts/{idArtifact}
-    
-    func deleteArtifact(_ req: Request) throws -> String {
-        throw "Not implemented"
-    }
-    
-    //GET 'artifacts/{idArtifact}/file')
-    func artifactDownloadInfo(_ req: Request) throws -> String {
-        throw "Not implemented"
-    }
-    
-    //PUT artifacts/{idArtifact}/file
-    func uploadArtifact(_ req: Request) throws -> String {
-        return "OK"
-    }
-    
-    
-    
-    //MARK: - ApiKey methods
-    
-    //    @ApiMethod( method: 'POST', path: 'artifacts/{apiKey}/{_branch}/{_version}/{_artifactName}')
-    
-    //    @ApiMethod(method: 'DELETE', path: 'artifacts/{apiKey}/{_branch}/{_version}/{_artifactName}')
-    
-    
-    //@ApiMethod(method: 'POST', path: 'artifacts/{apiKey}/last/{_artifactName}')
-    
-    // @ApiMethod(method: 'DELETE', path: 'artifacts/{apiKey}/last/{_artifactName}')
-    
-    //@ApiMethod(method: 'GET', path: 'app/{appId}/icon')
-    
-    //@ApiMethod(method: 'GET', path: 'artifacts/{idArtifact}/ios_plist')
-    
-    
-    func uploadArtifactOLD(_ req: Request) throws -> Future<String> {
-        struct UploadArtifact:Content {
-            let artifactFile: File
-            let sortIdentifier:String?
-            let jsonTags:String?
         }
-        print("Upload Done")
-        let artifactFile = try req.content.decode(UploadArtifact.self,maxSize:1024*1024*1024*1024)
-            .map({ artifact -> String in
-                print("Upload Artifact \(artifact)")
-                return "OK"
-            })
+        .flatMap{ artifact in
+            guard let artifact = artifact else { throw  Abort(.serviceUnavailable, reason: "Artifact not found for ID") }
+            return try retrieveArtifactData(artifact: artifact, storage: storage, into: context)
+                //return req.response("not implemented", as: .xml)
+                .flatMap{ storeResult  in
+                    /*  guard let mediaType = MediaType.parse(artifact.contentType ?? "") else { throw  Abort(.internalServerError, reason: "invalid Artifact mime Type") }
+                     response.http.contentType = mediaType*/
+                    let response:Response
+                    switch storeResult {
+                    case .asFile(let file):
+                        ()
+                        response = req.response("not implemented", as: .xml)
+                    //TO DO
+                    case .asUrI(let url):
+                        if url.scheme == "file"{ //local files
+                            return try req.streamFile(at: url.absoluteString)
+                        }else {
+                            //redirect to it
+                            response = req.redirect(to: url.absoluteString)
+                        }
+                    }
+                    return req.eventLoop.newSucceededFuture(result: response)
+                    //let response = req.response()
+                    
+                    //response.http.body =
+                    //   return req.response("not implemented", as: .xml)
+                    //throw "not implemented"
+            }
+        }
         
-        return artifactFile
-        /*  .flatMap { artifact -> EventLoopFuture<T> in
-         print(artifact)
-         
-         }*/
     }
 }
