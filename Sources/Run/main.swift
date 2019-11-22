a867323d8abdde3b5f3e5c0a423bcadbcf516d20
Rémi Groult <>
2019-11-22 16:12:15 +0100

    Add signal trap

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,7 +1,34 @@
 import App
+import Foundation
+import Vapor
+
+var mdtApp:Application!
+func closeApp(){
+    try? mdtApp.runningServer?.close().wait()
+       mdtApp.shutdownGracefully { error in
+           guard let error = error else { return }
+           print("Error on shutDown :\(error)")
+       }
+}
+
+signal(SIGINT) { signal in
+    print("SIGINT received")
+   closeApp()
+}
+
+signal(SIGKILL) { signal in
+    print("SIGKILL received")
+    closeApp()
+}
+
+signal(SIGTERM) { signal in
+    print("SIGTERM received")
+    closeApp()
+}
 
 do {
-    try app(.detect()).run()
+    mdtApp = try app(.detect())
+    try mdtApp.run()
 }catch {
     print("Unexpected error :\(error)")
 }
